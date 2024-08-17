6ba4f3f02591fd6cdfa2f011221a073e16542d49
Akiseweed <pier_terrazas@hotmail.com>
2024-08-16 19:15:03 -0500

    mayus

---
+++ mayus-minusculas.py
@@ -0,0 +1,9 @@
+"""
+pasar de Mayusculas a minusculas en 
+"""
+def Convertidor():
+    mayuscula=str(input("Ingrese una palabra solo en Mayuscula: "))#acá se pondrá toda palabra en mayuscula 
+    minuscula= mayuscula.lower()#
+    print(f"La palabra convertida a minuscula es: {minuscula}")
+Convertidor()
+    # if mayuscula != 
